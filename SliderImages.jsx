52c6909b37669c60390fde98d0d667373ef5d683
MarioKrajinovic <mario.krajinovic900@gmail.com>
2023-11-23 15:28:15 +0100

    Adding SliderImages.jsx

---
+++ SliderImages.jsx
@@ -0,0 +1,29 @@
+export const sliderImages = [
+    {
+       url: `./images/image1.png`,
+    },
+    {
+       url: `./images/image2.png`,
+    },
+    {
+       url: `./images/image3.png`,
+    },
+    {
+       url: `./images/image4.png`,
+    },
+    {
+       url: `./images/image5.png`,
+    },
+    {
+       url: `./images/image6.png`,
+    },
+    {
+       url: `./images/image7.png`,
+    },
+    {
+      url: `./images/image8.png`,
+   },
+   {
+      url: `./images/image9.png`,
+   },
+ ]; 
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -5,108 +5,71 @@ import img1 from "../images/roast1.jpeg";
 import img2 from "../images/roast2.jpg";
 import img3 from "../images/roast3.jpeg";
 import missionImage from "../images/mission.jpg"
-import image1 from "../images/image1.png"
-import image2 from "../images/image2.png"
-import image3 from "../images/image3.png"
-import image4 from "../images/image4.png"
-import image5 from "../images/image5.png"
-import image6 from "../images/image6.png"
-import image7 from "../images/image7.png"
-import image8 from "../images/image8.png"
-import image9 from "../images/image9.png"
 import { useMediaQuery } from 'react-responsive'
 import ModalImage from "./ModalImage";
+import {sliderImages} from "../SliderImages"
 
 export default function Home(){
    const mobile = useMediaQuery({ query: '(max-width:425px)'})
 
-   const sliderImages = [
-      {
-         url: `${image1}`,
-      },
-      {
-         url: `${image2}`,
-      },
-      {
-         url: `${image3}`,
-      },
-      {
-         url: `${image4}`,
-      },
-      {
-         url: `${image5}`,
-      },
-      {
-         url: `${image6}`,
-      },
-      {
-         url: `${image7}`,
-      },
-      {
-        url: `${image8}`,
-     },
-     {
-        url: `${image9}`,
-     },
-   ];   
-    return(
-        <div id="home">
-            <div className="image-slider">
-                <SimpleImageSlider
-                     images={sliderImages}
-                     width="100%"
-                     height={mobile ? "75vh" : "79vh"}
-                     showBullets={true}
-                     autoPlay={true}
-                     autoPlayDelay={7}
-                />
+   return(
+      <div id="home">
+         <div className="image-slider">
+            <SimpleImageSlider
+                  images={sliderImages}
+                  width="100%"
+                  height={mobile ? "75vh" : "79vh"}
+                  showBullets={true}
+                  autoPlay={true}
+                  autoPlayDelay={7}
+            />
+         </div>
+         <div className="welcome">
+            <div className="welcome-background">
+               <h1 data-aos="fade-up">WELCOME TO FAST FOOD, REIMAGINED.</h1>
+               <p data-aos="zoom-in" data-aos-duration="500">Healthy meals should be as delicious and affordable as traditional fast food. So we’re working on that. </p>
+               <p data-aos="zoom-in" data-aos-duration="500">We're bringing healthy cuisine to the world of fast food to help you eat a little better, a little more often.</p>
+               <p data-aos="zoom-in" data-aos-duration="500">It's fast.</p>
+               <p data-aos="zoom-in" data-aos-duration="500">It's cheap.</p>
+               <p data-aos="zoom-in" data-aos-duration="500">It's healthy.</p>
+               <p data-aos="zoom-in" data-aos-duration="500">It's “OMG SO GOOD I WANNA KEEP ORDERING” good. (in the kind words of a customer)</p>
+               <Link data-aos="fade-up" to="locations" target="_blank">Order Direct Online</Link>
             </div>
-            <div className="welcome">
-                <div className="welcome-background">
-                    <h1 data-aos="fade-up">WELCOME TO FAST FOOD, REIMAGINED.</h1>
-                    <p data-aos="zoom-in" data-aos-duration="500">Healthy meals should be as delicious and affordable as traditional fast food. So we’re working on that. </p>
-                    <p data-aos="zoom-in" data-aos-duration="500">We're bringing healthy cuisine to the world of fast food to help you eat a little better, a little more often.</p>
-                    <p data-aos="zoom-in" data-aos-duration="500">It's fast.</p>
-                    <p data-aos="zoom-in" data-aos-duration="500">It's cheap.</p>
-                    <p data-aos="zoom-in" data-aos-duration="500">It's healthy.</p>
-                    <p data-aos="zoom-in" data-aos-duration="500">It's “OMG SO GOOD I WANNA KEEP ORDERING” good. (in the kind words of a customer)</p>
-                    <Link data-aos="fade-up" to="locations" target="_blank">Order Direct Online</Link>
-                </div>
-            </div>
-            <div className="slow-roast-bowls">
-               <div className="slow-bg">
-                  <h1 data-aos="fade-up">THE SLOW ROAST BOWLS</h1>
-                  <p data-aos="zoom-in">
-                     every bowl includes slow roasted protein, a base of fresh greens or hearty grains (or both!), choice of signature sauce,
-                     steamed cauliflower, broccoli, carrots, roasted sunflower seeds, pickled red onions, fresh cucumber, & parsley garnish
-                  </p>
-                  <div className="roast-bowls">
-                     <div data-aos="fade-up">
-                        <Link to="menu"><img src={img1}/></Link>
-                        <h4>SLOW ROASTED PORK SHOULDER</h4>
-                        <p>slow roasted until it's fall-off-the-bone melty tender. no shortcuts to get there: just gentle heat, lots of time, and simple seasoning.</p>
-                     </div>
-                     <div data-aos="fade-up">
-                        <Link to="menu"><img src={img2}/></Link>
-                        <h4>SLOW ROASTED CHICKEN</h4>
-                        <p>juicy chicken slow roasted for hours with BFF seasoning until it's just perfectly succulent. this flavor only comes slowly.</p>
-                     </div>
-                     <div data-aos="fade-up">
-                        <Link to="menu"><img src={img3}/></Link>
-                        <h4>SLOW ROASTED CHICKPEA & POTATO</h4>
-                        <p>protein rich organic chickpeas & potatoes slow roasted with extra virgin olive oil and BFF seasoning. fudgy perfection.</p>
-                     </div>
+         </div>
+         <div className="slow-roast-bowls">
+            <div className="slow-bg">
+               <h1 data-aos="fade-up">THE SLOW ROAST BOWLS</h1>
+               <p data-aos="zoom-in">
+                  every bowl includes slow roasted protein, a base of fresh greens or hearty grains (or both!), choice of signature sauce,
+                  steamed cauliflower, broccoli, carrots, roasted sunflower seeds, pickled red onions, fresh cucumber, & parsley garnish
+               </p>
+               <div className="roast-bowls">
+                  <div data-aos="fade-up">
+                     <Link to="menu"><img src={img1}/></Link>
+                     <h4>SLOW ROASTED PORK SHOULDER</h4>
+                     <p>slow roasted until it's fall-off-the-bone melty tender. no shortcuts to get there: just gentle heat, lots of time, and simple seasoning.</p>
                   </div>
-                  <div className="allergies">
-                     <p data-aos="fade-right" className="title"><b>Allergies or dietary restrictions? We got you.</b></p>
-                     <p data-aos="fade-right"><b>Gluten, soy, dairy, fish allergy</b>? Rejoice! Our entire menu is gluten, dairy, soy, and fish free* (trace amounts from suppliers with cross-processing facilities may be present) </p>
-                     <p data-aos="fade-right"><b>Vegan, vegetarian</b>? Eat away! Everything on our menu aside from the meat is plant based.</p>
-                     <p data-aos="fade-right"><b>Low carb / keto</b>? Bon appetit! Our garden bowls are designed with you in mind - light on carbs, full on protein and healthy fats.</p>
+                  <div data-aos="fade-up">
+                     <Link to="menu"><img src={img2}/></Link>
+                     <h4>SLOW ROASTED CHICKEN</h4>
+                     <p>juicy chicken slow roasted for hours with BFF seasoning until it's just perfectly succulent. this flavor only comes slowly.</p>
                   </div>
-                  <div className="roast-buttons">
-                     <Link data-aos="fade-up" to="locations" target="_blank">Order Online</Link>
-                     <Link data-aos="fade-up" to="menu">See the Menu</Link>
+                  <div data-aos="fade-up">
+                     <Link to="menu"><img src={img3}/></Link>
+                     <h4>SLOW ROASTED CHICKPEA & POTATO</h4>
+                     <p>protein rich organic chickpeas & potatoes slow roasted with extra virgin olive oil and BFF seasoning. fudgy perfection.</p>
                   </div>
+               </div>
+               <div className="allergies">
+                  <p data-aos="fade-right" className="title"><b>Allergies or dietary restrictions? We got you.</b></p>
+                  <p data-aos="fade-right"><b>Gluten, soy, dairy, fish allergy</b>? Rejoice! Our entire menu is gluten, dairy, soy, and fish free* (trace amounts from suppliers with cross-processing facilities may be present) </p>
+                  <p data-aos="fade-right"><b>Vegan, vegetarian</b>? Eat away! Everything on our menu aside from the meat is plant based.</p>
+                  <p data-aos="fade-right"><b>Low carb / keto</b>? Bon appetit! Our garden bowls are designed with you in mind - light on carbs, full on protein and healthy fats.</p>
+               </div>
+               <div className="roast-buttons">
+                  <Link data-aos="fade-up" to="locations" target="_blank">Order Online</Link>
+                  <Link data-aos="fade-up" to="menu">See the Menu</Link>
+               </div>
             </div>
             <div className="home-mission">
                <div className="mission-text" data-aos="fade-right">
